3cacb3d9e91d2e0a4f4158eb0aa812962e1f262c
zhangxh21 <zhangxh21@asiainfo.com>
2023-04-04 02:07:25 +0800

    日志分割应用

---
+++ alone/log_cut/src/test/java/com/example/log_cut/LogCutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.log_cut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogCutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
