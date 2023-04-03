722fa9cf4f81b20f8d2ef8bd7dcc27b77fde7fe4
zhangxh21 <zhangxh21@asiainfo.com>
2023-04-04 01:52:12 +0800

    first commit

---
+++ src/main/java/com/example/zxh_structure/ZxhStructureApplication.java
@@ -0,0 +1,13 @@
+package com.example.zxh_structure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZxhStructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZxhStructureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zxh_structure/ZxhStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zxh_structure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZxhStructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
